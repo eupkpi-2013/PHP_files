7abba7b63fe5c3371488200aa53389434946aa73
Jasper Cacbay <jasper.cacbay@gmail.com>
2013-05-10 20:47:53 +0800

    fgjk

---
+++ kpi/auditor_home.php
@@ -0,0 +1,17 @@
+<div id="user-contents" class="contents">
+	<h1>Welcome, Auditor</h1>
+	<div>
+		<ul>Unverified KPI submissions:
+		<li><a href="auditor_verify_user001.html">User001</a></li>
+		<li><a href="auditor_verify_user002.html">User002</a></li>
+		<li><a href="auditor_verify_user003.html">User003</a></li>
+		</ul>
+	</div>
+	<div>
+		<ul>Recent reports:
+		<li><a href="auditor_results.html">Report1</a></li>
+		<li><a href="auditor_results.html">Report2</a></li>
+		<li><a href="auditor_results.html">Report3</a></li>
+		</ul>
+	</div>
+</div>
