7059ec2aac89c8603fad588d84b242ac0a76137d
Michelle Dela Cruz <delacruz.mv@gmail.com>
2013-05-14 08:35:30 +0800

    updated_user rate

---
+++ user_rate.php
@@ -0,0 +1,73 @@
+<div id="user-contents" class="contents">	
+	<div id="user-kpimenu" class="accordion lefted">
+		<?php foreach ($kpi as $kpi_item): 
+			 echo "<div><h3>".$kpi_item['kpi_name']."</h3><ul>";
+				 foreach ($subkpi as $subkpi_item): 
+					
+						if($subkpi_item['parent_kpi']==$kpi_item['kpi_id'])
+						{
+							$url1 = str_replace(" ", "_", $kpi_item['kpi_name']);
+							$url2 = str_replace(" ", "_", $subkpi_item['kpi_name']);
+							 echo "<div><li><a href='rate?q=".$url1."/".$url2."'>".$subkpi_item['kpi_name']."</a></div></li>";
+						}
+					
+				 endforeach; 
+				 
+			 echo "</ul></div>";
+		      endforeach; 
+		?>
+	</div>	
+	
+	<form action="add_user_rate" method="post">
+	<div id="user-inside" class="lefted">
+		
+		<table>
+		<?php		
+			if($checker!='empty'):
+				foreach ($metric as $metric_item): 
+		
+				
+				 foreach ($subkpi as $subkpi_item): 
+						 
+							if($subkpi_item['kpi_id']==$metric_item['kpi_id'])
+							 {
+								foreach ($kpi as $kpi_item):
+									if($kpi_item['kpi_id']==$subkpi_item['parent_kpi'])
+									{
+										$parent = $kpi_item['kpi_name'];
+									}
+								endforeach;
+								
+								echo "<h2>".$parent." > ".$subkpi_item['kpi_name']."</h2>";
+					         }
+						
+				 endforeach; 
+				 
+					$id = $metric_item['field_id'];
+					echo "<tr><td>".$metric_item['field_name']."<td><td><input type='text' name='metric_item[]'></input></td></tr>";	
+					echo "<input type='hidden' name='id[]' value='$id'></input>";
+				endforeach;		
+		
+			else:
+				echo "<h2>eUP KPI: After 2 months</h2><p>Choose a KPI on the left.</p><br><button>View your previous ratings</button>";
+		
+		
+		    endif; ?>
+		</table>
+		
+		<?php
+			if($checker!='empty'): 
+				echo "<button class='righted'>Next</button>";
+		
+		 endif; ?>
+		
+	</div>
+	</form>
+</div>
+
+<!--javascripts-->
+<script>
+
+
+</script>
+
