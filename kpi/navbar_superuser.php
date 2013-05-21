779fe60fb716258440fa5194c96aa53b6e1b8e33
Johnrene Magalona <j.magalona12@gmail.com>
2013-05-21 10:29:52 +0800

    target
    target

--- kpi/navbar_superuser.php
+++ kpi/navbar_superuser.php
@@ -3,6 +3,7 @@
 			<li id="user-home"><a href="superuser_home.html">Home</a></li>
 			<li id="user-rate"><a href="superuser_kpis.html">KPIs</a></li>
 			<li id="user-rate"><a href="superuser_accounts.html">Accounts</a></li>
+			<li id="user-rate"><a href="superuser_targets.html">Targets</a></li>
 			<li id="user-results"><a href="superuser_results.html">Results</a></li>
 			</ul>
 		</div>
